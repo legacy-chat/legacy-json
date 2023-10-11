afd10806a76191648e5aed275b738090bc65bf17
Meep3692 <meep3692@gmail.com>
2023-10-10 22:26:57 -0400

    JsonSerializationException now gives better message

--- src/main/java/ca/awoo/json/JsonSerializationException.java
+++ src/main/java/ca/awoo/json/JsonSerializationException.java
@@ -4,22 +4,22 @@ public class JsonSerializationException extends JsonException {
     public Object obj;
 
     public JsonSerializationException(Object obj) {
-        super("Could not serialize object: " + obj);
+        super("Could not serialize object: " + obj.toString() + " with type: " + obj.getClass().getName());
         this.obj = obj;
     }
 
     public JsonSerializationException(Object obj, String message) {
-        super(message + ": " + obj);
+        super(message + ": " + obj.toString() + " with type: " + obj.getClass().getName());
         this.obj = obj;
     }
 
     public JsonSerializationException(Object obj, Throwable cause) {
-        super("Could not serialize object: " + obj, cause);
+        super("Could not serialize object: " + obj.toString() + " with type: " + obj.getClass().getName(), cause);
         this.obj = obj;
     }
 
     public JsonSerializationException(Object obj, String message, Throwable cause) {
-        super(message + ": " + obj, cause);
+        super(message + ": " + obj.toString() + " with type: " + obj.getClass().getName(), cause);
         this.obj = obj;
     }
 }
